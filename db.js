f06c0990da914c0ca69de7a3d3f9c85b0167a0d9
Abhishek <abhipatel8719@gmail.com>
2021-09-02 17:31:54 -0700

    dfslkfjs

--- db.js
+++ db.js
@@ -3,6 +3,9 @@ import pool from "pg";
 console.log(process.env.DATABASE_URL);
 export default new pool.Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   // connectionString:
   // "postgres://postgres:*Abhishekpatel09042000@localhost:5432/blockchain_db",
 });
